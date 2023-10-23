a66a41478d0adc9cf2ef27d693e90cbb295b8b59
nagar-ajay <ajay.nagar@nutanix.com>
2023-10-23 17:06:21 +0530

    add support to pull docker images using docker secrets

--- kubeflow/install.sh
+++ kubeflow/install.sh
@@ -5,14 +5,16 @@ KF_VERSION=v1.8.0-rc.1
 helpFunction()
 {
     echo ""
-    echo "Usage: install.sh [OPTIONAL -v]"
+    echo "Usage: install.sh [OPTIONAL -v] [OPTIONAL -d]"
     echo "-v    vanilla kubeflow"
+    echo "-d    use docker credentials"
     exit 1 # Exit script after printing help
 }
 
-while getopts ":v" option; do
+while getopts "vd" option; do
     case $option in
         v ) vanilla_kubeflow="vanilla_kubeflow" ;;
+        d ) use_docker_creds="use_docker_creds" ;;
         ? ) helpFunction ;;
     esac
 done
@@ -32,6 +34,26 @@ then
     cp overlays/pipeline/pipeline-kustomization.yaml manifests/apps/pipeline/upstream/env/cert-manager/platform-agnostic-multi-user/kustomization.yaml
     mkdir -p manifests/apps/pipeline/upstream/env/ntnx
     cp -r overlays/pipeline/ntnx manifests/apps/pipeline/upstream/env
+fi
+
+if [ -n "$use_docker_creds"  ]
+then
+    echo "Using docker imagePullSecrets"
+    source overlays/docker/docker-credentials.env
+    kubectl create namespace kubeflow
+    kubectl create namespace istio-system
+    kubectl create secret docker-registry kf-docker-cred --docker-server=$DOCKER_SERVER --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --docker-email=$DOCKER_EMAIL -n kubeflow
+    kubectl create secret docker-registry kf-docker-cred --docker-server=$DOCKER_SERVER --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --docker-email=$DOCKER_EMAIL -n istio-system
+    kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "kf-docker-cred"}]}' -n kubeflow
+
+    cp overlays/docker/service-account-patch.yaml manifests/example/service-account-patch.yaml
+
+    cat << EOF >> manifests/example/kustomization.yaml
+
+patchesStrategicMerge:
+- service-account-patch.yaml
+EOF
+
 fi
 
 # Install kubeflow
